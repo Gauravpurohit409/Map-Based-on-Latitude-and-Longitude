4a8c31c6934b168788db89806473c0d422dc3680
Gaurav <gauravpurohit409@gmail.com>
2019-04-29 18:30:31 +0530

    marker is added

--- find_a_Place.py
+++ find_a_Place.py
@@ -1,19 +1,30 @@
 import tkinter as tk
 import folium 
 import webbrowser
-
-def execute():
-	a = float(entry.get())
+def execute():				#this function fetches the value for longitude and latitude 
+	a = float(entry.get())	#and then open saved html file in web browser
 	b = float(entry1.get())
 	map = folium.Map(location = [a,b])
 	map.save("Map.html")
 	url =  "file:///E:/web/python/project_1/Map.html"
 	webbrowser.open(url,new = 1)
 
+def marker():				#when user want to mark a location at that time this fun will mark it 
+	a = float(entry.get())	#and then open saved html file in web browser
+	b = float(entry1.get())
+	fg = folium.FeatureGroup(name = "My map")
+	fg.add_child(folium.Marker(location= [a,b],popup = "Welcome to Earth",icon = folium.Icon(color = "green")))
+	map = folium.Map(location = [a,b])
+	map.add_child(fg)
+	map.save("Map.html")
+	url =  "file:///E:/web/python/project_1/Map.html"
+	webbrowser.open(url,new = 1)
+
+
 root = tk.Tk();
 
 f = tk.Frame(root,height = 50,width = 50)
-f.pack()
+f.grid()
 
 L1 = tk.Label(f,text = "Enter the latitude")
 L1.grid(row=0,column = 0)
@@ -28,8 +39,12 @@ entry1 = tk.Entry(f)
 entry1.grid(row = 1,column =1)
 
 
-button = tk.Button(root,text = "Search",command = execute)
+button = tk.Button(root,text = "   Search   ",command = execute)
 button.bind("<Button -1>",execute)
-button.pack()
+button.grid(row = 3,column = 0)
+
+markerbutton = tk.Button(root,text = "Mark Location", command = marker)
+markerbutton.bind("<Button-1>",marker)
+markerbutton.grid(row = 4,column = 0)
 
 root.mainloop()
